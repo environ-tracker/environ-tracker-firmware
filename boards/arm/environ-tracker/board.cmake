8a2a070458fa58a1b011ae87435064b3f362c046
Aaron Helmore <s4482152@student.uq.edu.au>
2022-03-12 22:48:20 +1000

    added correct support for environ-tracker board

---
+++ boards/arm/environ-tracker/board.cmake
@@ -0,0 +1,6 @@
+board_runner_args(pyocd "--target=nrf52833" "--frequency=4000000")
+board_runner_args(nrfjprog "--softreset")
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
