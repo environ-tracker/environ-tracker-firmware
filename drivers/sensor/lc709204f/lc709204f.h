ff5be3cc33c9283174636f9cdc9608cc339532ec
Aaron Helmore <aaron@helmore.org>
2022-08-05 13:32:57 +1000

    refactor to improve readability

--- drivers/sensor/lc709204f/lc709204f.h
+++ drivers/sensor/lc709204f/lc709204f.h
@@ -1,76 +1,130 @@
 #ifndef _LC709204F_H_
 #define _LC709204F_H_
 
-/* LC709204F register addresses */
-#define LC709204F_REG_TIME_TO_EMPTY             0x03
-#define LC709204F_REG_BEFORE_RSOC               0x04
-#define LC709204F_REG_TIME_TO_FULL              0x05
-#define LC709204F_REG_TSENSE1_THERM_B           0x06
-#define LC709204F_REG_INITIAL_RSOC              0x07
-#define LC709204F_REG_CELL_TEMP                 0x08
-#define LC709204F_REG_CELL_VOLTAGE              0x09
-#define LC709204F_REG_CURRENT_DIRECTION         0x0A
-#define LC709204F_REG_APA                       0x0B
-#define LC709204F_REG_APT                       0x0C
-#define LC709204F_REG_RSOC                      0x0D
-#define LC709204F_REG_TSENSE2_THERM_B           0x0E
-#define LC709204F_REG_ITE                       0x0F
-#define LC709204F_REG_IC_VERSION                0x11
-#define LC709204F_REG_CHANGE_OF_PARAMETER       0x12
-#define LC709204F_REG_ALARM_LOW_RSOC            0x13
-#define LC709204F_REG_ALARM_LOW_VOLTAGE         0x14
-#define LC709204F_REG_IC_POWER_MODE             0x15
-#define LC709204F_REG_STATUS_BIT                0x16
-#define LC709204F_REG_CYCLE_COUNT               0x17
-#define LC709204F_REG_BATTERY_STATUS            0x19
-#define LC709204F_REG_NUMBER_OF_PARAMETER       0x1A
-#define LC709204F_REG_TERMINATION_CURRENT_RATE  0x1C
-#define LC709204F_REG_EMPTY_CELL_VOLTAGE        0x1D
-#define LC709204F_REG_ITE_OFFSET                0x1E
-#define LC709204F_REG_ALARM_HIGH_VOLTAGE        0x1F
-#define LC709204F_REG_ALARM_LOW_TEMP            0x20
-#define LC709204F_REG_ALARM_HIGH_TEMP           0x21
-#define LC709204F_REG_TOTAL_RUN_TIME_LOW_2B     0x24
-#define LC709204F_REG_TOTAL_RUN_TIME_HIGH_2B    0x25
-#define LC709204F_REG_ACCUM_TEMPERATURE_LOW_2B  0x26
-#define LC709204F_REG_ACCUM_TEMPERATURE_HIGH_2B 0x27
-#define LC709204F_REG_ACCUM_RSOC_LOW_2B         0x28
-#define LC709204F_REG_ACCUM_RSOC_HIGH_2B        0x29
-#define LC709204F_REG_MAX_CELL_VOLTAGE          0x2A
-#define LC709204F_REG_MIN_CELL_VOLTAGE          0x2B
-#define LC709204F_REG_MAX_CELL_TEMP             0x2C
-#define LC709204F_REG_MIN_CELL_TEMP             0x2D
-#define LC709204F_REG_AMBIENT_TEMP              0x30
-#define LC709204F_REG_STATE_OF_HEALTH           0x32
-#define LC709204F_REG_USER_ID_LOW_2B            0x36
-#define LC709204F_REG_USER_ID_HIGH_2B           0x37
+/* Commands */
+enum lc709204f_commands {
+    TIME_TO_EMPTY               = 0x03,
+    BEFORE_RSOC                 = 0x04,
+    REG_TIME_TO_FULL            = 0x05,
+    TSENSE1_THERM_B             = 0x06,
+    INITIAL_RSOC                = 0x07,
+    CELL_TEMP                   = 0x08,
+    CELL_VOLTAGE                = 0x09,
+    CURRENT_DIRECTION           = 0x0A,
+    APA                         = 0x0B,
+    APT                         = 0x0C,
+    RSOC                        = 0x0D,
+    TSENSE2_THERM_B             = 0x0E,
+    ITE                         = 0x0F,
+    IC_VERSION                  = 0x11,
+    CHANGE_OF_PARAMETER         = 0x12,
+    ALARM_LOW_RSOC              = 0x13,
+    ALARM_LOW_VOLTAGE           = 0x14,
+    IC_POWER_MODE               = 0x15,
+    STATUS_BIT                  = 0x16,
+    CYCLE_COUNT                 = 0x17,
+    BATTERY_STATUS              = 0x19,
+    NUMBER_OF_PARAMETER         = 0x1A,
+    TERMINATION_CURRENT_RATE    = 0x1C,
+    EMPTY_CELL_VOLTAGE          = 0x1D,
+    ITE_OFFSET                  = 0x1E,
+    ALARM_HIGH_VOLTAGE          = 0x1F,
+    ALARM_LOW_TEMP              = 0x20,
+    ALARM_HIGH_TEMP             = 0x21,
+    TOTAL_RUN_TIME_LOW_2B       = 0x24,
+    TOTAL_RUN_TIME_HIGH_2B      = 0x25,
+    ACCUM_TEMPERATURE_LOW_2B    = 0x26,
+    ACCUM_TEMPERATURE_HIGH_2B   = 0x27,
+    ACCUM_RSOC_LOW_2B           = 0x28,
+    ACCUM_RSOC_HIGH_2B          = 0x29,
+    MAX_CELL_VOLTAGE            = 0x2A,
+    MIN_CELL_VOLTAGE            = 0x2B,
+    MAX_CELL_TEMP               = 0x2C,
+    MIN_CELL_TEMP               = 0x2D,
+    AMBIENT_TEMP                = 0x30,
+    STATE_OF_HEALTH             = 0x32,
+    USER_ID_LOW_2B              = 0x36,
+    USER_ID_HIGH_2B             = 0x37,
+};
 
 /* LC709204F RSOC init commands */
-#define LC709204F_CMD_RSOC_INIT_1_SAMPLE        0xAA55
-#define LC709204F_CMD_RSOC_INIT_2_SAMPLE        0xAA56
-#define LC709204F_CMD_RSOC_INIT_3_SAMPLE        0xAA57
-#define LC709204F_CMD_RSOC_INIT_4_SAMPLE        0xAA58
+enum lc709204f_rsoc_init_commands {
+    SAMPLE_1    = 0xAA55,
+    SAMPLE_2    = 0xAA56,
+    SAMPLE_3    = 0xAA57,
+    SAMPLE_4    = 0xAA58,
+};
 
-/* LC709204F battery types */
-#define LC709204F_BATTERY_01    0x00
-#define LC709204F_BATTERY_04    0x01
-#define LC709204F_BATTERY_05    0x02
-#define LC709204F_BATTERY_06    0x03
-#define LC709204F_BATTERY_07    0x04
+/* LC709204F supported battery types */
+enum lc709204f_supported_battery_types {
+    BATTERY_01  = 0x00,
+    BATTERY_04  = 0x01,
+    BATTERY_05  = 0x02,
+    BATTERY_06  = 0x03,
+    BATTERY_07  = 0x04,
+};
 
-/* LC709204F status bit controls */
-#define LC709204F_TSENSE1_CTRL  (1 << 0)
-#define LC709204F_TSENSE2_CTRL  (1 << 1)
+/* Status bit masks */
+enum lc709204f_status_bit_masks {
+    TSENSE1_CTRL        = 1 << 0,
+    TSENSE2_CTRL        = 1 << 1,
+    STATUS_DISCHARGING  = 1 << 6,
+    STATUS_INITIALIZED  = 1 << 7,
+    ALARM_LOW_TEMP      = 1 << 8,
+    ALARM_LOW_RSOC      = 1 << 9,
+    ALARM_LOW_VOLTAGE   = 1 << 11,
+    ALARM_HIGH_TEMP     = 1 << 12,
+    ALARM_HIGH_VOLTAGE  = 1 << 15,
+};
 
-/* LC709204F battery status bits */
-#define LC709204F_STATUS_DISCHARGING    (1 << 6)
-#define LC709204F_STATUS_INITIALIZED    (1 << 7)
+struct lc709204f_data {
+    const struct device *i2c;
+    /* Current cell voltage in mV */
+    uint16_t voltage;
+    /* Max cell voltage seen in mV */
+    uint16_t max_voltage;
+    /* Min cell voltage seen in mV */
+    uint16_t min_voltage;
+    /* Remaining capacity in units of 0.1% */
+    uint16_t state_of_charge;
+    /* State of health of the cell in units of 1% */
+    uint16_t state_of_health;
+    /* Time to full in minutes */
+    uint16_t time_to_full;
+    /* Time to empty in minutes */
+    uint16_t time_to_empty;
+    /* Cycle count (number of charge and discharge cycles) */
+    uint16_t cycle_count;
+    /* Current cell temperature in 0.1 degrees C offset by +0xAAC */
+    uint16_t cell_temp;
+    /* Max cell temperature seen in 0.1 degrees C offset by +0xAAC */
+    uint16_t max_cell_temp;
+    /* Min cell temperature seen in 0.1 degrees C offset by +0xAAC */
+    uint16_t min_cell_temp;
+    /* Current ambient temperature */
+    uint16_t ambient_temp;
+    /* Total run time from POR in minutes */
+    uint32_t total_run_time;
+};
 
-/* LC709204F alarm status bits */
-#define LC709204F_ALARM_LOW_TEMP        (1 << 8)
-#define LC709204F_ALARM_LOW_RSOC        (1 << 9)
-#define LC709204F_ALARM_LOW_VOLTAGE     (1 << 11)
-#define LC709204F_ALARM_HIGH_TEMP       (1 << 12)
-#define LC709204F_ALARM_HIGH_VOLTAGE    (1 << 15)
+struct lc709204f_config {
+    char *bus_name;
+    /* Design capacity (label capacity) of the cell in mAh */
+    uint16_t design_capacity;
+    /* Design capacity of the cell in mV */
+    uint16_t design_voltage;
+    /* Desired voltage of the cell in mV */
+    uint16_t desired_voltage;
+    /* Empty voltage of the cell in mV */
+    uint16_t empty_voltage;
+    /* Desired charging current in mA */
+    uint16_t desired_charging_current;
+    /* Charging termination current in uA */
+    uint16_t charging_termination_current;
+    /* Adjustment parameters */
+    uint16_t apa_value;
+    /* Battery type (a type supported by the LC709204F) */
+    enum lc709204f_supported_battery_types battery_type;
+};
 
 #endif /* _LC709204F_H_ */
