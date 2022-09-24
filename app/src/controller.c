4498c40e8c5804dfa0ed35573531eafa60abfb9c
Aaron Helmore <aaron@helmore.org>
2022-09-24 14:24:08 +1000

    Moved button init code to sys_init function

--- app/src/controller.c
+++ app/src/controller.c
@@ -8,68 +8,11 @@ LOG_MODULE_REGISTER(controller);
 #define CONTROLLER_STACK_SIZE  2048
 #define CONTROLLER_PRIORITY    3
 
-#define SW0_NODE    DT_ALIAS(sw0)
-#define SW1_NODE    DT_ALIAS(sw1)
-#define SW2_NODE    DT_ALIAS(sw2)
 
-static const struct gpio_dt_spec button0 = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
-static const struct gpio_dt_spec button1 = GPIO_DT_SPEC_GET(SW1_NODE, gpios);
-static const struct gpio_dt_spec button2 = GPIO_DT_SPEC_GET(SW2_NODE, gpios);
-
-static struct gpio_callback button0_cb_data;
-static struct gpio_callback button1_cb_data;
-static struct gpio_callback button2_cb_data;
-
-void button_pressed(const struct device *dev, struct gpio_callback *cb, 
-        uint32_t pins)
-{
-    LOG_ERR("Button pressed, pins: %d", pins);
-}
-
-static int init_button(const struct gpio_dt_spec *button, 
-        struct gpio_callback *callback)
-{
-    int ret;
-
-    if (!device_is_ready(button->port)) {
-        LOG_ERR("Button device %s is not ready", button->port->name);
-        return -ENODEV;
-    }
-
-    ret = gpio_pin_configure_dt(button, GPIO_INPUT);
-    if (ret != 0) {
-        LOG_ERR("Failed to configure GPIO on %s pin %d (%d)", 
-                button->port->name, button->pin, ret);
-        return ret;
-    }
-
-    ret = gpio_pin_interrupt_configure_dt(button, GPIO_INT_EDGE_TO_INACTIVE);
-    if (ret != 0) {
-        LOG_ERR("Failed to configure interrupt on %s pin %d (%d)", 
-                button->port->name, button->pin, ret);
-        return ret;
-    }
-
-    gpio_init_callback(callback, button_pressed, BIT(button->pin));
-    gpio_add_callback(button->port, callback);
-
-    return 0;
-}
 
 void controller_thread(void *a, void *b, void *c)
 {
-    int ret;
 
-    LOG_INF("Controller starting");
-
-    ret = init_button(&button0, &button0_cb_data);
-    ret |= init_button(&button1, &button1_cb_data);
-    ret |= init_button(&button2, &button2_cb_data);
-    if (ret != 0) {
-        return;
-    }
-
-    LOG_INF("Controller started.");
 }
 
-K_THREAD_DEFINE(controller_id, CONTROLLER_STACK_SIZE, controller_thread, NULL, NULL, NULL, CONTROLLER_PRIORITY, 0, 0);
+// K_THREAD_DEFINE(controller_id, CONTROLLER_STACK_SIZE, controller_thread, NULL, NULL, NULL, CONTROLLER_PRIORITY, 0, 0);
--- app/src/sys_init.c
+++ app/src/sys_init.c
@@ -2,6 +2,7 @@
 #include <device.h>
 #include <posix/time.h>
 #include <drivers/counter.h>
+#include <drivers/gpio.h>
 
 #include <logging/log_ctrl.h>
 #include <logging/log.h>
@@ -21,6 +22,55 @@ LOG_MODULE_REGISTER(init);
 #if INIT_INIT_PRIORITY < CONFIG_SENSOR_INIT_PRIORITY
 #error "INIT_INIT_PRIORITY must be larger than SENSOR_INIT_PRIORITY"
 #endif
+
+
+#define SW0_NODE    DT_ALIAS(sw0)
+#define SW1_NODE    DT_ALIAS(sw1)
+#define SW2_NODE    DT_ALIAS(sw2)
+
+static const struct gpio_dt_spec button0 = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+static const struct gpio_dt_spec button1 = GPIO_DT_SPEC_GET(SW1_NODE, gpios);
+static const struct gpio_dt_spec button2 = GPIO_DT_SPEC_GET(SW2_NODE, gpios);
+
+static struct gpio_callback button0_cb_data;
+static struct gpio_callback button1_cb_data;
+static struct gpio_callback button2_cb_data;
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb, 
+        uint32_t pins)
+{
+    LOG_INF("Button pressed, pin: %d", pins);
+}
+
+static int init_button(const struct gpio_dt_spec *button, 
+        struct gpio_callback *callback)
+{
+    int ret;
+
+    if (!device_is_ready(button->port)) {
+        LOG_ERR("Button device %s is not ready", button->port->name);
+        return -ENODEV;
+    }
+
+    ret = gpio_pin_configure_dt(button, GPIO_INPUT);
+    if (ret != 0) {
+        LOG_ERR("Failed to configure GPIO on %s pin %d (%d)", 
+                button->port->name, button->pin, ret);
+        return ret;
+    }
+
+    ret = gpio_pin_interrupt_configure_dt(button, GPIO_INT_EDGE_TO_INACTIVE);
+    if (ret != 0) {
+        LOG_ERR("Failed to configure interrupt on %s pin %d (%d)", 
+                button->port->name, button->pin, ret);
+        return ret;
+    }
+
+    gpio_init_callback(callback, button_pressed, BIT(button->pin));
+    gpio_add_callback(button->port, callback);
+
+    return 0;
+}
 
 
 int init(const struct device *dev)
@@ -52,7 +102,7 @@ int init(const struct device *dev)
         return rc;
     }
 
-    LOG_DBG("Realtime clock set to RTC time");
+    LOG_INF("Realtime clock set to RTC time");
 
     backend = log_backend_get_by_name("log_backend_fs");
     
@@ -66,6 +116,18 @@ int init(const struct device *dev)
     }
     
     LOG_INF("FS backend set to LOG_LEVEL_WRN");
+
+
+    rc = init_button(&button0, &button0_cb_data);
+    rc |= init_button(&button1, &button1_cb_data);
+    rc |= init_button(&button2, &button2_cb_data);
+    if (rc != 0) {
+        LOG_ERR("Error while initialising buttons. (%d)", rc);
+        return rc;
+    }
+
+    LOG_INF("Buttons initialised");
+
     return 0;
 }
 
