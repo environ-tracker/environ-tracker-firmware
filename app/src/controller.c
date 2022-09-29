1f5daaf40f2a923607ebf9d8384dbe529cba21e0
Aaron Helmore <aaron@helmore.org>
2022-09-30 00:33:52 +1000

    Add boot_count incrementing to accumulator

--- app/src/controller.c
+++ app/src/controller.c
@@ -1,18 +1,53 @@
 #include <zephyr.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
-
 #include <logging/log.h>
+
+#include "file/file_common.h"
+
+
 LOG_MODULE_REGISTER(controller);
+
 
 #define CONTROLLER_STACK_SIZE  2048
 #define CONTROLLER_PRIORITY    3
 
 
+/**
+ * @brief Increment the systems boot counter
+ * 
+ * @return 0 on success, else negative error code from FS API
+ */
+int increment_boot_count(void)
+{
+    uint32_t boot_count = 0;
+    int err    
+
+    err = read_file("boot_count", (uint8_t *)&boot_count, sizeof(boot_count));
+	if (err == -EEXIST) {
+		boot_count = 0;
+	} else if (err != 0) {
+        return err;
+    } else {
+		LOG_INF("boot_count is: %d", boot_count);
+		++boot_count;
+	}
+
+	err = write_file("boot_count", (uint8_t *)&boot_count, sizeof(boot_count));
+	
+    return err;
+}
+
 
 void controller_thread(void *a, void *b, void *c)
 {
+    LOG_INF("Controller started");
 
+    increment_boot_count();
+
+    // TODO: start other threads
+
+    while (1) {
+        k_msleep(100);
+    }
 }
 
-// K_THREAD_DEFINE(controller_id, CONTROLLER_STACK_SIZE, controller_thread, NULL, NULL, NULL, CONTROLLER_PRIORITY, 0, 0);
+K_THREAD_DEFINE(controller_id, CONTROLLER_STACK_SIZE, controller_thread, NULL, NULL, NULL, CONTROLLER_PRIORITY, 0, 0);
