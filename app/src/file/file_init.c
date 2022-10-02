1c4356a4fe40c8f2bf2360fdfc3e499056062fa9
Aaron Helmore <aaron@helmore.org>
2022-10-02 21:09:16 +1000

    Fixed beacon file init function

--- app/src/file/file_init.c
+++ app/src/file/file_init.c
@@ -6,13 +6,18 @@
 LOG_MODULE_DECLARE(file);
 
 #include "file/file_common.h"
+#include "ble/ble_network.h"
 
-#ifdef APP_CONFIG_BEACON_FILES
+
+#ifdef CONFIG_APP_INIT_BEACON_FILES
 #include <bluetooth/uuid.h>
 /* NOTE: This is currently the only supported iBeacon network UUID */
 static const struct bt_uuid_128 supported_beacon_network = BT_UUID_INIT_128(
         BT_UUID_128_ENCODE(0x7aaf1b67, 0xf3c0, 0x4a54, 0xb314, 0x58fff1960a40));
-#endif /* APP_CONFIG_BEACON_FILES */
+
+
+extern struct fs_mount_t *mp;
+#endif /* CONFIG_APP_INIT_BEACON_FILES */
 
 
 #ifdef CONFIG_APP_FLASH_ERASE
@@ -43,96 +48,84 @@ int storage_flash_erase(unsigned int id)
 }
 #endif /* CONFIG_APP_FLASH_ERASE */
 
-#ifdef APP_CONFIG_BEACON_FILES
-int initialise_files(struct fs_mount_t *mp)
+#ifdef CONFIG_APP_INIT_BEACON_FILES
+int initialise_files(void)
 {
-    struct fs_file_t file;
-    char fname[64], fname2[64];
-    char uuid[50];
-    char buffer[200];
+    char file_name[FILE_NAME_LEN], dir_name[FILE_NAME_LEN];
+    char uuid[BT_UUID_STR_LEN];
 
-    // NOTE: I think the endianess of these is wrong
-    /* This is 4 beacons in the form: majorminor,x,y,z */
-    uint8_t file_contents[] = {0, 0, 0, 0, ',', 0, ',', 0, ',', 0, '\n',
-                               0, 0, 0, 1, ',', 10, ',', 0, ',', 0, '\n',
-                               0, 0, 0, 3, ',', 0, ',', 10, ',', 0, '\n',
-                               0, 0, 0, 4, ',', 10, ',', 10, ',', 0, '\n'};
+    struct ibeacon_file_info file_contents[] = {
+        {
+            .id = BEACON_ID_INIT(0, 0),
+            .location = {
+                .longitude = 0,
+                .latitude = 0,
+                .altitude = 0
+            }
+        },
+        {
+            .id = BEACON_ID_INIT(0, 1),
+            .location = {
+                .longitude = 10,
+                .latitude = 0,
+                .altitude = 0
+            }
+        },
+        {
+            .id = BEACON_ID_INIT(0, 2),
+            .location = {
+                .longitude = 0,
+                .latitude = 10,
+                .altitude = 0
+            }
+        },
+        {
+            .id = BEACON_ID_INIT(0, 3),
+            .location = {
+                .longitude = 10,
+                .latitude = 10,
+                .altitude = 0
+            }
+        },
+        {
+            .id = BEACON_ID_INIT(0, 4),
+            .location = {
+                .longitude = 20,
+                .latitude = 5,
+                .altitude = 0
+            }
+        }
+    };
+
     int err, ret;
 
     LOG_INF("Initialising files");
 
-    ret = snprintf(fname2, sizeof(fname2), "%s/beacons", mp->mnt_point);
-    if (ret > sizeof(fname2)) {
+    ret = absolute_file_name(dir_name, "beacons");
+    if (ret) {
+        LOG_ERR("initialise_files: Error forming beacon dir name. (%d)", ret);
+        return ret;
+    }
+
+    err = fs_mkdir(dir_name);
+    if (err != 0 && err != -EEXIST) {
+        LOG_ERR("Failed to make directory: %s. Error: %d", dir_name, err);
+        return err;
+    }
+
+    bt_uuid_to_str(&supported_beacon_network.uuid, uuid, sizeof(uuid));
+    ret = snprintf(file_name, sizeof(file_name), "beacons/%s", uuid);
+    if (ret > sizeof(file_name)) {
         LOG_WRN("Truncated filename, size would be: %d", ret);
         return ret;
     }
 
-    err = fs_mkdir(fname2);
-    if (err != 0 && err != -EEXIST) {
-        LOG_ERR("Failed to make directory: %s. Error: %d", log_strdup(fname2), 
-                err);
-        return err;
+    ret = write_file(file_name, (uint8_t *)&file_contents, 
+            sizeof(file_contents));
+    if (ret) {
+        LOG_ERR("initialise_files: Error initialising beacon file. (%d)", ret);
     }
 
-    bt_uuid_to_str(&supported_beacon_network.uuid, uuid, 50);
-    ret = snprintf(fname, sizeof(fname), "%s/%s.bin", fname2, uuid);
-    if (ret > sizeof(fname)) {
-        LOG_WRN("Truncated filename, size would be: %d", ret);
-        return ret; // TODO: change to a better errno
-    }
-
-    fs_file_t_init(&file);
-    err = fs_open(&file, fname, FS_O_CREATE | FS_O_RDWR);
-    if (err < 0) {
-        LOG_ERR("Failed to open file %s. Error: %d", log_strdup(fname), err);
-        return err;
-    }
-
-    err = fs_write(&file, file_contents, ARRAY_SIZE(file_contents));
-    if (err < 0) {
-        LOG_ERR("Failed to write to file %s. Error: %d", log_strdup(fname), err);
-        goto out;
-    }
-
-    if (err != ARRAY_SIZE(file_contents)) {
-        LOG_ERR("Didn't write entire array. Wrote %d bytes, array is %d bytes", err, ARRAY_SIZE(file_contents));
-        goto out;
-    }
-
-    err = fs_sync(&file);
-    if (err < 0) {
-        LOG_ERR("Couldn't flush file. Error %d", err);
-        goto out;
-    }
-
-    err = fs_seek(&file, 0, FS_SEEK_SET);
-    if (err < 0) {
-        LOG_ERR("Failed to seek to start of file, Error %d", err);
-        goto out;
-    }
-
-    err = fs_read(&file, buffer, 200);
-    if (err < 0) {
-        LOG_ERR("Failed while reading. Error %d", err);
-        goto out;
-    }
-
-    int offset;
-    for (int i = 0; i < 4; ++i) {
-        offset = i * 11;
-        LOG_PRINTK("Major: %x%x, Minor: %x%x, x: %x, y: %x, z: %x\n", 
-                buffer[offset + 0], buffer[offset + 1], buffer[offset + 2], 
-                buffer[offset + 3], buffer[offset + 5], buffer[offset + 7], 
-                buffer[offset + 9]);
-    }
-
-out:
-    err = fs_close(&file);
-    if (err < 0) {
-        LOG_ERR("Failed to close file. Error %d", err);
-        return err;
-    }
-
-    return (err < 0 ? err : 0);
+    return ret;
 }
-#endif /* APP_CONFIG_BEACON_FILES */
+#endif /* CONFIG_APP_INIT_BEACON_FILES */
