3c929083dfddc7f32d9c9e6d3d68362384dd73d5
Aaron Helmore <aaron@helmore.org>
2022-10-15 22:33:05 +1000

    Documentation and renaming

--- app/src/init/gpio_init.c
+++ app/src/init/gpio_init.c
@@ -37,6 +37,12 @@ static struct gpio_callback button1_cb_data;
 static struct gpio_callback button2_cb_data;
 
 
+/**
+ * @brief Maps the physical pin number of a button to its logical button number
+ * 
+ * @param pin Physcial pin number
+ * @return Logical button number
+ */
 static enum button_map map_pin(uint32_t pin) 
 {
     if (pin == BIT(button0.pin)) {
@@ -50,7 +56,11 @@ static enum button_map map_pin(uint32_t pin)
     }
 }
 
-
+/**
+ * @brief Callback for system buttons
+ *
+ * @param pins Bitfield of triggering pins
+ */
 static void button_pressed(const struct device *dev, struct gpio_callback *cb, 
         uint32_t pins)
 {
@@ -83,43 +93,56 @@ static void button_pressed(const struct device *dev, struct gpio_callback *cb,
     }
 }
 
-static int init_button(const struct gpio_dt_spec *button, 
+
+/**
+ * @brief Initialised a given GPIO as a double edge triggered interrupt source
+ * 
+ * @param gpio GPIO to initialise as interrupt source
+ * @param callback Callback for the GPIO to call
+ * @return 0 on success, else negative error code
+ */
+static int init_gpio(const struct gpio_dt_spec *gpio, 
         struct gpio_callback *callback)
 {
     int ret;
 
-    if (!device_is_ready(button->port)) {
-        LOG_ERR("Button device %s is not ready", button->port->name);
+    if (!device_is_ready(gpio->port)) {
+        LOG_ERR("Button device %s is not ready", gpio->port->name);
         return -ENODEV;
     }
 
-    ret = gpio_pin_configure_dt(button, GPIO_INPUT);
+    ret = gpio_pin_configure_dt(gpio, GPIO_INPUT);
     if (ret != 0) {
         LOG_ERR("Failed to configure GPIO on %s pin %d (%d)", 
-                button->port->name, button->pin, ret);
+                gpio->port->name, gpio->pin, ret);
         return ret;
     }
 
-    ret = gpio_pin_interrupt_configure_dt(button, GPIO_INT_EDGE_BOTH);
+    ret = gpio_pin_interrupt_configure_dt(gpio, GPIO_INT_EDGE_BOTH);
     if (ret != 0) {
         LOG_ERR("Failed to configure interrupt on %s pin %d (%d)", 
-                button->port->name, button->pin, ret);
+                gpio->port->name, gpio->pin, ret);
         return ret;
     }
 
-    gpio_init_callback(callback, button_pressed, BIT(button->pin));
-    gpio_add_callback(button->port, callback);
+    gpio_init_callback(callback, button_pressed, BIT(gpio->pin));
+    gpio_add_callback(gpio->port, callback);
 
     return 0;
 }
 
+/**
+ * @brief Initialises GPIO
+ * 
+ * @return 0 on success, else negative error code
+ */
 int gpio_init(const struct device *dev)
 {
     int rc;
 
-    rc = init_button(&button0, &button0_cb_data);
-    rc |= init_button(&button1, &button1_cb_data);
-    rc |= init_button(&button2, &button2_cb_data);
+    rc = init_gpio(&button0, &button0_cb_data);
+    rc |= init_gpio(&button1, &button1_cb_data);
+    rc |= init_gpio(&button2, &button2_cb_data);
     if (rc != 0) {
         LOG_ERR("Error while initialising buttons. (%d)", rc);
         return rc;
